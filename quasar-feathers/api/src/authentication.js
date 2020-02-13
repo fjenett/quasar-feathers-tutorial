61b03df53707b741879828e9f25841d767b66896
Florian Jenett <fjenett@gmail.com>
2020-02-13 09:17:51 +0100

    trying to fix JWT

--- quasar-feathers/api/src/authentication.js
+++ quasar-feathers/api/src/authentication.js
@@ -1,10 +1,10 @@
-const { AuthenticationService /* , JWTStrategy */ } = require('@feathersjs/authentication');
+const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 
 module.exports = app => {
   const authentication = new AuthenticationService(app);
 
-  // authentication.register('jwt', new JWTStrategy());
+  authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
 
   app.use('/authentication', authentication);
--- quasar-feathers/api/src/services/users/users.hooks.js
+++ quasar-feathers/api/src/services/users/users.hooks.js
@@ -1,6 +1,6 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { hashPassword, protect } = require('@feathersjs/authentication-local').hooks;
-const commonHooks  = require('feathers-hooks-common');
+const commonHooks = require('feathers-hooks-common');
 const gravatar = require('../../hooks/gravatar');
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
     all: [],
     find: [ authenticate('jwt') ],
     get: [ authenticate('jwt') ],
-    create: [hashPassword('passwordField'), gravatar()],
+    create: [hashPassword('password'), gravatar()],
     update: [ authenticate('jwt') ],
     patch: [ authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
